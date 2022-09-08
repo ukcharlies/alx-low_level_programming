9b5e40ddc3d0e4b1925ad0cbd14c3cacd051ff46
ukcharlies <ukcharlies@gmail.com>
2022-09-08 21:31:28 +0000

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *
  *Return:if no error return zero, otherwise return non zero value
  */
-int main(void);
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n.");
 return (0);
