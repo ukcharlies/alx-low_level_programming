7214adb766ec6b238eda8ea932fa6dc38f98acde
ukcharlies <ukcharlies@gmail.com>
2022-09-08 21:12:56 +0000

    new changes in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  *Return:if no error return zero, otherwise return non zero value
  */
-int main(void)
+int main(void);
 {
-printf("with proper grammar, but the outcome is a piece of art,\n.")
-return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n.");
+return (0);
 }
