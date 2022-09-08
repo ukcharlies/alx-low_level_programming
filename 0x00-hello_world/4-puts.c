69678f680f107bff84cc2df3a9dff34eb1fd1186
ukcharlies <ukcharlies@gmail.com>
2022-09-08 20:03:41 +0000

    added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>								
+/**
+ *main-Serves as the entry piont of a program
+ *initialize the strings 
+ *write the strings out
+ *return-if no error return zero, otherrwise return non zero value
+ */
+int main(void)
+{
+char word[]="\"Programming is like building a multilingual puzzle";
+puts(word);
+returns (0);
+}
