a0d549f0772b02617aba9ab26f057a3e70405501
ukcharlies <ukcharlies@gmail.com>
2022-09-08 20:42:18 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>								
+#include <stdio.h>
 /**
  *main-Serves as the entry piont of a program
- *initialize the strings 
+ *initialize the strings
  *write the strings out
- *return-if no error return zero, otherrwise return non zero value
+ *Return:if no error return zero, otherrwise return non zero value
  */
 int main(void)
 {
-char word[]="\"Programming is like building a multilingual puzzle";
+char word[] = "\"Programming is like building a multilingual puzzle";
 puts(word);
-returns (0);
+return (0);
 }
