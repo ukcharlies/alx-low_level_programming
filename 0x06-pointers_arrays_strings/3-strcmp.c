bc7f8bc9b9365b048f8ecf819e2ca82c6bb91e39
ukcharlies <ukcharlies@gmail.com>
2022-09-22 21:17:07 +0000

    edit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,8 @@
  * @s2: A pointer to the second string to be compared.
  *
  * Return: If str1 < str2, the negative difference
- * 	If str1 == str2, 0.
- * 	If str1 > str2, the positive difference
+ *	If str1 == str2, 0.
+ *	If str1 > str2, the positive difference
  */
 
 int _strcmp(char *s1, char *s2)
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 /**
  * string_toupper - Changes all lowercase letters
- * 		of a string to uppercase.
+ *		of a string to uppercase.
  * @str: The string to be changed.
  *
  * Return: A pointer to the changed string.
